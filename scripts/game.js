42424b158fcd59380c9daab416c01442dd01269e
AlexGTraining <alex.gavrilut.training@gmail.com>
2025-02-07 17:47:47 +0200

    Added varied sized obstacles

--- scripts/game.js
+++ scripts/game.js
@@ -1,6 +1,7 @@
 const PLAYER = document.querySelector(".player");
 const ORIGINAL_OBSTACLE = document.querySelector(".obstacle");
-const SCORE_DISPLAY = document.querySelector('.score-display');
+const SCORE_DISPLAY = document.querySelector('.score');
+const TOP_SCORE_DISPLAY = document.querySelector('.top-score');
 
 const BODY_COMPUTED_STYLE = window.getComputedStyle(document.body);
 const CANVAS_WIDTH = parseInt(BODY_COMPUTED_STYLE.getPropertyValue('--canvas-width'));
@@ -8,8 +9,10 @@ const PLAYER_WIDTH = parseInt(BODY_COMPUTED_STYLE.getPropertyValue('--player-wid
 const PLAYER_JUMP_DURATION = parseInt(BODY_COMPUTED_STYLE.getPropertyValue('--jump-duration'));
 const PLAYER_JUMP_HEIGHT = parseInt(BODY_COMPUTED_STYLE.getPropertyValue('--jump-height'));
 const OBSTACLE_MOVE_DURATION = parseInt(BODY_COMPUTED_STYLE.getPropertyValue('--obstacle-move-duration'));
-const OBSTACLE_HEIGHT = parseInt(BODY_COMPUTED_STYLE.getPropertyValue('--obstacle-height'));
-const OBSTACLE_WIDTH = parseInt(BODY_COMPUTED_STYLE.getPropertyValue('--obstacle-width'));
+const OBSTACLE_NORMAL_HEIGHT = parseInt(BODY_COMPUTED_STYLE.getPropertyValue('--obstacle-height'));
+const OBSTACLE_TALL_HEIGHT = parseInt(BODY_COMPUTED_STYLE.getPropertyValue('--obstacle-tall-height'));
+const OBSTACLE_NORMAL_WIDTH = parseInt(BODY_COMPUTED_STYLE.getPropertyValue('--obstacle-width'));
+const OBSTACLE_WIDE_WIDTH = parseInt(BODY_COMPUTED_STYLE.getPropertyValue('--obstacle-wide-width'));
 const OBSTACLES_SPAWN_FREQUENCY = 2500;
 
 const GAME_INSTRUCTION_ELEMENT = document.querySelector('.instruction-text');
@@ -24,14 +27,43 @@ const sounds = {
     collision: document.querySelector('.collisionSound')
 };
 
+const INTRO_HYPE_MESSAGES = ['READY', 'SET', 'GO!', 'Press SPACE to JUMP'];
+const TIME_BETWEEN_MESSAGES = 1000;
+
+let introMessageCompleted = 0;
+let obstacle_widths = [];
+let obstacle_heights = [];
+
 document.body.onkeyup = function (e) {
     if (e.code == "Enter" || e.keyCode == 13) {
         reset();
     }
 
     if (e.key == " " || e.code == "Space" || e.keyCode == 32) {
-        jump();
-        startGame();
+
+        if (has_game_started) {
+            jump();
+            return;
+        }
+
+        if (introMessageCompleted == 0) {
+            for (let i = 0; i < INTRO_HYPE_MESSAGES.length; i++) {
+                TIMEOUTS.push(setTimeout(() => {
+                    GAME_INSTRUCTION_ELEMENT.classList.add("intro");
+
+                    showInstruction(INTRO_HYPE_MESSAGES[introMessageCompleted]);
+
+                    if (introMessageCompleted == INTRO_HYPE_MESSAGES.length - 1)
+                        startGame();
+
+                    introMessageCompleted++;
+                }, TIME_BETWEEN_MESSAGES * i));
+
+                TIMEOUTS.push(setTimeout(() => {
+                    GAME_INSTRUCTION_ELEMENT.classList.remove("intro");
+                }, i == 0 ? 300 : 1300 * i));
+            }
+        }
     }
 }
 
@@ -41,10 +73,11 @@ const startGame = function () {
     sounds.background.play()
     if (has_game_started) return;
 
+    GAME_INSTRUCTION_ELEMENT.classList.remove("intro");
+
     spawnPlayer();
     spawnObstacles();
     has_game_started = true;
-    showInstruction('Press SPACE to JUMP');
 }
 
 const gameOver = function () {
@@ -59,16 +92,22 @@ const gameOver = function () {
     PLAYER.classList.add('dead');
 
     showInstruction('GAME OVER');
-    setTimeout(() => showInstruction('Press ENTER to RESET'), 2000);
+    TIMEOUTS.push(setTimeout(() => showInstruction('Press ENTER to RESET'), 2000));
 
-    console.log("gameOver");
+    if (score > top_score)
+        top_score = score;
+
+    updateScoreDisplay();
+
     is_game_over = true;
 }
 
 const jump = function () {
     if (is_player_jumping || !has_game_started)
         return;
-    sounds.jump.play();
+
+    if (!is_game_over)
+        sounds.jump.play();
 
 
     removeClass('animate_player', PLAYER);
@@ -90,11 +129,13 @@ const reset = function () {
         element.classList.remove('pause');
     });
 
+    GAME_INSTRUCTION_ELEMENT.classList.remove("intro");
     PLAYER.classList.remove('jump');
     PLAYER.classList.remove('pause');
     PLAYER.classList.remove('animate_player');
     PLAYER.classList.remove('dead');
 
+    introMessageCompleted = 0;
     is_player_in_hight_danger = true;
     is_player_in_side_danger = false;
     has_game_started = false;
@@ -104,7 +145,6 @@ const reset = function () {
     updateScoreDisplay();
     showInstruction('Press SPACE to START');
 
-    console.log("reset");
     is_game_over = false;
 }
 
@@ -164,10 +204,36 @@ const initObstacle = function (obstacle) {
 
     VISUALS.forEach(visual => obstacle.classList.remove(visual));
 
-    obstacle.classList.add(VISUALS[Math.floor(Math.random() * VISUALS.length)]);
+    let obstacleType = VISUALS[Math.floor(Math.random() * VISUALS.length)];
+    obstacle.classList.add(obstacleType);
+    obstacle.classList.remove('obstacle-normal', 'obstacle-tall', 'obstacle-wide');
 
-    let startPosition = OBSTACLES.indexOf(obstacle) * OBSTACLE_WIDTH;
-    let endPosition = (CANVAS_WIDTH - OBSTACLE_WIDTH - startPosition) * -1;
+    if (obstacle_widths.length >= OBSTACLES.length)
+        obstacle_widths.splice(0, 1);
+
+    if (obstacle_heights.length >= OBSTACLES.length)
+        obstacle_heights.splice(0, 1);
+
+    switch (obstacleType) {
+        case VISUALS[0]:
+            obstacle_widths.push(OBSTACLE_NORMAL_WIDTH);
+            obstacle_heights.push(OBSTACLE_NORMAL_HEIGHT);
+            addClass('obstacle-normal', obstacle);
+            break;
+        case VISUALS[1]:
+            obstacle_widths.push(OBSTACLE_WIDE_WIDTH);
+            obstacle_heights.push(OBSTACLE_NORMAL_HEIGHT);
+            addClass('obstacle-wide', obstacle);
+            break;
+        case VISUALS[2]:
+            obstacle_widths.push(OBSTACLE_NORMAL_WIDTH);
+            obstacle_heights.push(OBSTACLE_TALL_HEIGHT);
+            addClass('obstacle-tall', obstacle);
+            break;
+    }
+
+    let startPosition = obstacle_widths.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+    let endPosition = (CANVAS_WIDTH - obstacle_widths.slice(-1) - startPosition) * -1;
     obstacle.style.setProperty('--obstacle-start-position', `${startPosition}px`);
     obstacle.style.setProperty('--obstacle-move-distance', `${endPosition}px`);
     addClass('move', obstacle);
@@ -218,15 +284,18 @@ const incrementScore = function () {
 
 const updateScoreDisplay = function () {
     SCORE_DISPLAY.innerText = `Score: ${score}`;
+    TOP_SCORE_DISPLAY.innerText = `TOP Score: ${top_score}`;
 }
 
 const calculateTimeToSideImpact = function () {
-    let result = ((CANVAS_WIDTH - PLAYER_WIDTH - OBSTACLE_WIDTH / 2) / CANVAS_WIDTH) * OBSTACLE_MOVE_DURATION;
+    let width = obstacle_widths.slice(-1);
+    let result = ((CANVAS_WIDTH - PLAYER_WIDTH - width / 2) / CANVAS_WIDTH) * OBSTACLE_MOVE_DURATION;
     return result * 1000;
 }
 
 const calculateTimeToHightSafety = function () {
-    let result = ((OBSTACLE_HEIGHT / PLAYER_JUMP_HEIGHT) * (PLAYER_JUMP_DURATION / 2));
+    let height = obstacle_heights.slice(-1);
+    let result = ((height / PLAYER_JUMP_HEIGHT) * (PLAYER_JUMP_DURATION / 2));
     return result * 1000;
 }
 
@@ -238,6 +307,7 @@ const removeClass = function (item, element) {
     if (element.classList.contains(item)) element.classList.remove(item);
 }
 
+let top_score = 0;
 let score = 0;
 let has_game_started = false;
 let is_game_over = false;
